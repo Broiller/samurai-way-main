0a0c30d16ac47a88b736e77840ac32d4dde2770a
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-01-03 03:13:12 +0300

    add lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header"
+
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className='app-wrapper'>
+            <header className='header'> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1200px-Adidas_Logo.svg.png?20220310161517" alt='a'/> </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='a'/>
+            </div>
+            <div>
+                <img src="https://inspirats.com/images/2014/08/23/images-26825-27541-hd-wallpapers.jpg" alt="w"/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
         </div>
     );
 }

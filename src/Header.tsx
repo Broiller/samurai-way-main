bdcd84efd4def366f8a4d35b70a86a901dc17dc9
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-01-04 06:16:25 +0300

    add lesson 12

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 const Header = () => {
     return (
-        <div >
-            <a href='#s'>Home</a>
-            <a href='#s'>News Feed</a>
-            <a href='#s'>Messages</a>
-        </div>
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/1200px-Adidas_Logo.svg.png?20220310161517" alt='a'/>
+        </header>
+
     );
 }
 
---
+++ src/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='a'/>
+            <div>
+                <img src="https://inspirats.com/images/2014/08/23/images-26825-27541-hd-wallpapers.jpg" alt="w"/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;

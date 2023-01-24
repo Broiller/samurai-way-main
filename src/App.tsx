171e596dd5a15befcfb6fbd0a6ae19294ce2dc88
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-01-25 02:46:40 +0300

    add lesson 29

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import {BrowserRouter, Route} from "react-router-dom";
 import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
 import {Settings} from "./components/Serrings/Settings";
+import state from "./redux/state";
+import {Friends} from "./components/Friends/Friends";
 
 
 const App = (props) => {
@@ -19,11 +21,13 @@ const App = (props) => {
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={() => <Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
-                    <Route path='/profile' render={() => <Profile posts={props.posts}/>}/>
+                    <Route path='/dialogs' render={() => <Dialogs state={props.state.messagesPage}/>}/>
+                    <Route path='/profile' render={() => <Profile state={props.state.profilePage}/>}/>
                     <Route path='/music' render={() => <Music/>}/>
                     <Route path='/news' render={() => <News/>}/>
                     <Route path='/settings' render={() => <Settings/>}/>
+                    {/*<Route path='/friends' render={() => <Friends/>}/>*/}
+
                 </div>
             </div>
         </BrowserRouter>
---
+++ src/redux/state.tsx
@@ -0,0 +1,39 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: "Hi, how are you?", likesCount: 12},
+            {id: 2, message: "It's my first post", likesCount: 11},
+            {id: 3, message: "!!!EEE", likesCount: 15},
+        ]
+
+    },
+    messagesPage: {
+        dialogs: [
+            {id: 1, name: "Dimych"},
+            {id: 2, name: "Andrew"},
+            {id: 3, name: "Sveta"},
+            {id: 4, name: "Sasha"},
+            {id: 5, name: "Victor"},
+            {id: 6, name: "Valera"}
+        ],
+        messages: [
+            {id: 1, message: "lol"},
+            {id: 2, message: "kek"},
+            {id: 3, message: "yo"},
+            {id: 4, message: "hi"},
+            {id: 5, message: "how"},
+            {id: 6, message: "psss"}
+        ]
+    },
+    // sidebar: {
+    //     friends: [
+    //         {id: 1, name: "Andrew"},
+    //         {id: 2, name: "Sasha"},
+    //         {id: 3, name: "Sveta"},
+    //
+    //     ]
+    //
+    // },
+}
+
+export default state

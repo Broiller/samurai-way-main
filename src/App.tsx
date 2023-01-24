371cf5a70326c61db0b0d98cadd6a52bdf1f03ed
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-01-25 02:58:25 +0300

    add lesson 29

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,16 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {Music} from "./components/Music/Music";
 import {News} from "./components/News/News";
 import {Settings} from "./components/Serrings/Settings";
-import state from "./redux/state";
-import {Friends} from "./components/Friends/Friends";
 
 
 const App = (props) => {
 
 
     return (
-        <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
@@ -27,10 +24,9 @@ const App = (props) => {
                     <Route path='/news' render={() => <News/>}/>
                     <Route path='/settings' render={() => <Settings/>}/>
                     {/*<Route path='/friends' render={() => <Friends/>}/>*/}
-
                 </div>
             </div>
-        </BrowserRouter>
+
     )
 }
 

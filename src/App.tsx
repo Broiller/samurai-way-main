8fa9b919d39f53e822f98d00f8e6aff74c093986
Yauheni Haurylenka <djonkaupervud@mail.ru>
2023-02-16 00:08:52 +0300

    add lesson 39

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,7 @@ const App = (props) => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={() => <Dialogs
-
-                               dialogsPage={props.state.dialogsPage}/>}/>
+                           render={() => <Dialogs state={props.state.dialogsPage}/>}/>
                     <Route path='/profile'
                            render={() => <Profile
                                profilePage={props.state.profilePage}
